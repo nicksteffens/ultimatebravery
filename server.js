d1984cd0447435f6866a58e9802cadc56a53d7b5
Nick <nsteffens@gmail.com>
2012-03-29 18:48:30 -0500

    removed json

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ http.createServer(function (request, response) {
         }
     });
      
-}).listen(3000);
+}).listen(3000, '127.0.0.1');
  
 console.log('Server running at http://127.0.0.1:3000/');
