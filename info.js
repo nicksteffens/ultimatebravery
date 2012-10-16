ca97b8b89d2c9e5c91d25ecd18efa326980d38e5
Nick Steffens <nick.steffens@designkitchen.com>
2012-10-16 17:11:55 -0500

    adding js object

---
+++ info.js
@@ -0,0 +1,426 @@
+info = {
+    "url_paths": {
+      "host": "http://na.leagueoflegends.com/sites/default/files/game_data/",
+      "game_data": "1.0.0.146",
+      "icons": "icons",
+      "portraits": "portraits"
+    },
+    "champions": [
+        {
+          "number": 103,
+          "name": "ahri"
+        },
+        {
+          "number": 84,
+          "name": "akali"
+        },
+        {
+          "number": 12,
+          "name": "alistar"
+        },
+        {
+          "number": 32,
+          "name": "amumu"
+        },
+        {
+          "number": 34,
+          "name": "anivia"
+        },
+        {
+          "number": 1,
+          "name": "annie"
+        },
+        {
+          "number": 22,
+          "name": "ashe"
+        },
+        {
+          "number": 53,
+          "name": "blitzcrank"
+        },
+        {
+          "number": 63,
+          "name": "brand"
+        },
+        {
+          "number": 51,
+          "name": "caitlyn"
+        },
+        {
+          "number": 69,
+          "name": "cassiopeia"
+        },
+        {
+          "number": 31,
+          "name": "cho"
+        },
+        {
+          "number": 42,
+          "name": "corki"
+        },
+        {
+          "number": 122,
+          "name": "darius"
+        },
+        {
+          "number": 131,
+          "name": "diana"
+        },
+        {
+          "number": 36,
+          "name": "dr"
+        },
+        {
+          "number": 119,
+          "name": "draven"
+        },
+        {
+          "number": 28,
+          "name": "evelynn"
+        },
+        {
+          "number": 81,
+          "name": "ezreal"
+        },
+        {
+          "number": 9,
+          "name": "fiddlesticks"
+        },
+        {
+          "number": 114,
+          "name": "fiora"
+        },
+        {
+          "number": 105,
+          "name": "fizz"
+        },
+        {
+          "number": 3,
+          "name": "galio"
+        },
+        {
+          "number": 41,
+          "name": "gangplank"
+        },
+        {
+          "number": 86,
+          "name": "garen"
+        },
+        {
+          "number": 79,
+          "name": "gragas"
+        },
+        {
+          "number": 104,
+          "name": "graves"
+        },
+        {
+          "number": 120,
+          "name": "hecarim"
+        },
+        {
+          "number": 74,
+          "name": "heimerdinger"
+        },
+        {
+          "number": 39,
+          "name": "irelia"
+        },
+        {
+          "number": 40,
+          "name": "janna"
+        },
+        {
+          "number": 59,
+          "name": "jarvan"
+        },
+        {
+          "number": 24,
+          "name": "jax"
+        },
+        {
+          "number": 126,
+          "name": "jayce"
+        },
+        {
+          "number": 43,
+          "name": "karma"
+        },
+        {
+          "number": 30,
+          "name": "karthus"
+        },
+        {
+          "number": 38,
+          "name": "kassadin"
+        },
+        {
+          "number": 55,
+          "name": "katarina"
+        },
+        {
+          "number": 10,
+          "name": "kayle"
+        },
+        {
+          "number": 85,
+          "name": "kennen"
+        },
+        {
+          "number": 96,
+          "name": "kog"
+        },
+        {
+          "number": 7,
+          "name": "leblanc"
+        },
+        {
+          "number": 64,
+          "name": "lee"
+        },
+        {
+          "number": 89,
+          "name": "leona"
+        },
+        {
+          "number": 117,
+          "name": "lulu"
+        },
+        {
+          "number": 99,
+          "name": "lux"
+        },
+        {
+          "number": 54,
+          "name": "malphite"
+        },
+        {
+          "number": 90,
+          "name": "malzahar"
+        },
+        {
+          "number": 57,
+          "name": "maokai"
+        },
+        {
+          "number": 11,
+          "name": "master"
+        },
+        {
+          "number": 21,
+          "name": "miss"
+        },
+        {
+          "number": 82,
+          "name": "mordekaiser"
+        },
+        {
+          "number": 25,
+          "name": "morgana"
+        },
+        {
+          "number": 75,
+          "name": "nasus"
+        },
+        {
+          "number": 111,
+          "name": "nautilus"
+        },
+        {
+          "number": 76,
+          "name": "nidalee"
+        },
+        {
+          "number": 56,
+          "name": "nocturne"
+        },
+        {
+          "number": 20,
+          "name": "nunu"
+        },
+        {
+          "number": 2,
+          "name": "olaf"
+        },
+        {
+          "number": 61,
+          "name": "orianna"
+        },
+        {
+          "number": 80,
+          "name": "pantheon"
+        },
+        {
+          "number": 78,
+          "name": "poppy"
+        },
+        {
+          "number": 33,
+          "name": "rammus"
+        },
+        {
+          "number": 58,
+          "name": "renekton"
+        },
+        {
+          "number": 107,
+          "name": "rengar"
+        },
+        {
+          "number": 92,
+          "name": "riven"
+        },
+        {
+          "number": 68,
+          "name": "rumble"
+        },
+        {
+          "number": 13,
+          "name": "ryze"
+        },
+        {
+          "number": 113,
+          "name": "sejuani"
+        },
+        {
+          "number": 35,
+          "name": "shaco"
+        },
+        {
+          "number": 98,
+          "name": "shen"
+        },
+        {
+          "number": 102,
+          "name": "shyvana"
+        },
+        {
+          "number": 27,
+          "name": "singed"
+        },
+        {
+          "number": 14,
+          "name": "sion"
+        },
+        {
+          "number": 15,
+          "name": "sivir"
+        },
+        {
+          "number": 72,
+          "name": "skarner"
+        },
+        {
+          "number": 37,
+          "name": "sona"
+        },
+        {
+          "number": 16,
+          "name": "soraka"
+        },
+        {
+          "number": 50,
+          "name": "swain"
+        },
+        {
+          "number": 134,
+          "name": "syndra"
+        },
+        {
+          "number": 91,
+          "name": "talon"
+        },
+        {
+          "number": 44,
+          "name": "taric"
+        },
+        {
+          "number": 17,
+          "name": "teemo"
+        },
+        {
+          "number": 18,
+          "name": "tristana"
+        },
+        {
+          "number": 48,
+          "name": "trundle"
+        },
+        {
+          "number": 23,
+          "name": "tryndamere"
+        },
+        {
+          "number": 4,
+          "name": "twisted"
+        },
+        {
+          "number": 29,
+          "name": "twitch"
+        },
+        {
+          "number": 77,
+          "name": "udyr"
+        },
+        {
+          "number": 6,
+          "name": "urgot"
+        },
+        {
+          "number": 110,
+          "name": "varus"
+        },
+        {
+          "number": 67,
+          "name": "vayne"
+        },
+        {
+          "number": 45,
+          "name": "veigar"
+        },
+        {
+          "number": 112,
+          "name": "viktor"
+        },
+        {
+          "number": 8,
+          "name": "vladimir"
+        },
+        {
+          "number": 106,
+          "name": "volibear"
+        },
+        {
+          "number": 19,
+          "name": "warwick"
+        },
+        {
+          "number": 62,
+          "name": "wukong"
+        },
+        {
+          "number": 101,
+          "name": "xerath"
+        },
+        {
+          "number": 5,
+          "name": "xin"
+        },
+        {
+          "number": 83,
+          "name": "yorick"
+        },
+        {
+          "number": 115,
+          "name": "ziggs"
+        },
+        {
+          "number": 26,
+          "name": "zilean"
+        },
+        {
+          "number": 143,
+          "name": "zyra"
+        }
+    ]
+};
